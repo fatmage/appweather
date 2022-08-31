64423f57545b4a6d71ad1de7e7cbbaf7fccd9a10
Michał Bednarz <michal.bednarz@riskmethods.net>
2022-08-31 10:00:07 +0200

    typo

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -10,7 +10,7 @@ class CityController < ApplicationController
     def show_city
 
         #uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&appid=#{Rails.application.credentials.open_weather[:appid]}")
-        ri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&appid=23833e88e1260c293773e3f1e59ce5e8")
+        uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&appid=23833e88e1260c293773e3f1e59ce5e8")
         @res = JSON.parse(Net::HTTP.get(uri))
 
         if @res.empty?
