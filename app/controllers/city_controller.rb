0dc1ca514b4708a286dc65ad3417448b454a1c1f
Michał Bednarz <michal.bednarz@riskmethods.net>
2022-08-31 11:07:51 +0200

    credentials

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -9,8 +9,8 @@ class CityController < ApplicationController
 
     def show_city
 
-        #uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&appid=#{Rails.application.credentials.open_weather[:appid]}")
-        uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&appid=23833e88e1260c293773e3f1e59ce5e8")
+        uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&appid=#{Rails.application.credentials.open_weather[:appid]}")
+        #uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{params[:city]}&appid=23833e88e1260c293773e3f1e59ce5e8")
         @res = JSON.parse(Net::HTTP.get(uri))
 
         if @res.empty?
